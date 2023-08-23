1a5a1b59789ad7cd1d9028085f08eb06e0a7718f
NuggetsTheCoder <142940180+NuggetsTheCoder@users.noreply.github.com>
2023-08-23 11:15:11 -0700

    Update test.py
    updated the problem of config wiping at the cost of additional setup

--- test.py
+++ test.py
@@ -15,35 +15,61 @@ num1 = str(input(f"{Style.RESET_ALL}what logo would you like?: "))
 
 
 if num1 == ("archlinux"):
+    oldfile = f = open("/home/" + username + "/.config/neofetch/stupid.conf", "r").read()
 
-    f = open("/home/" + username + "/.config/neofetch/config.conf", 'w')
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "w")
+    f.write(oldfile)
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "a")
     f.write("ascii_distro='archlinux'")
 
     print (Fore.GREEN + "Sucessful!")
 
 elif num1 == ("redhat"):
-    f = open("/home/" + username + "/.config/neofetch/config.conf", 'w')
+    oldfile = f = open("/home/" + username + "/.config/neofetch/stupid.conf", "r").read()
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "w")
+    f.write(oldfile)
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "a")
     f.write("ascii_distro='redhat'")
 
     print (Fore.GREEN + "Sucessful!")
 
 elif num1 == ("arcolinux"):
-    f = open("/home/" + username + "/.config/neofetch/config.conf", 'w')
+    oldfile = f = open("/home/" + username + "/.config/neofetch/stupid.conf", "r").read()
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "w")
+    f.write(oldfile)
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "a")
     f.write("ascii_distro='arcolinux'")
 
     print (Fore.GREEN + "Sucessful!")
-
+    
 elif num1 == ("fedora"):
-    f = open("/home/" + username + "/.config/neofetch/config.conf", 'w')
+    oldfile = f = open("/home/" + username + "/.config/neofetch/stupid.conf", "r").read()
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "w")
+    f.write(oldfile)
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "a")
     f.write("ascii_distro='fedora'")
 
     print (Fore.GREEN + "Sucessful!")
+    
 
 elif num1 == ("garuda"):
-    f = open("/home/" + username + "/.config/neofetch/config.conf", 'w')
-    f.write("ascii_distro='garuda")
+    oldfile = f = open("/home/" + username + "/.config/neofetch/stupid.conf", "r").read()
 
-    print (Fore.GREEN + "sucessful!")
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "w")
+    f.write(oldfile)
+
+    f = open("/home/" + username + "/.config/neofetch/config.conf", "a")
+    f.write("ascii_distro='garuda'")
+
+    print (Fore.GREEN + "Sucessful!")
+   
 
 
 num2 = str(input(f"{Style.RESET_ALL}would you like neofetch to automatically run when you boot up any kind of console? [y/n]: "))
@@ -51,6 +77,7 @@ num2 = str(input(f"{Style.RESET_ALL}would you like neofetch to automatically run
 if num2 == ("y"):
     f = open("/home/" + username + "/.config/neofetch/config.conf", 'a')
     f.write("neofetch")
+    
 
     os.system("gnome-terminal")
 
